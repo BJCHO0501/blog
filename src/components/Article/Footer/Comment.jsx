5d76d233e76f3fb20d6ad3daa9e1391e43300a24
Byung jin <bjcho1503@naver.com>
2025-09-05 03:27:38 +0900

    댓글 기능 설정

--- src/components/Article/Footer/Comment.jsx
+++ src/components/Article/Footer/Comment.jsx
@@ -8,21 +8,20 @@ const Comment = () => {
   const { theme } = useSelector(state => state.theme)
 
   return (
-    <></>
-    // <Giscus
-    //   id="comments"
-    //   repo={giscus.repo}
-    //   repoId={giscus.repoId}
-    //   category={giscus.category}
-    //   categoryId={giscus.categoryId}
-    //   mapping={giscus.mapping}
-    //   strict={giscus.strict}
-    //   reactionsEnabled={giscus.reactionsEnabled}
-    //   inputPosition={giscus.inputPosition}
-    //   lang={giscus.lang}
-    //   theme={theme === "light" ? "light" : "dark"}
-    //   emitMetadata="0"
-    // />
+    <Giscus
+      id="comments"
+      repo={giscus.repo}
+      repoId={giscus.repoId}
+      category={giscus.category}
+      categoryId={giscus.categoryId}
+      mapping={giscus.mapping}
+      strict={giscus.strict}
+      reactionsEnabled={giscus.reactionsEnabled}
+      inputPosition={giscus.inputPosition}
+      lang={giscus.lang}
+      theme={theme === "light" ? "light" : "dark"}
+      emitMetadata="0"
+    />
   )
 }
 
